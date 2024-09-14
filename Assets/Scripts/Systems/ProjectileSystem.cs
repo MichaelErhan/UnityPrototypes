6b7ce1a8eab4efa25a016312b80ad78b7225378b
Michael <mihaerhan01@mail.ru>
2024-09-14 20:21:43 +0300

    add base prototype

---
+++ Assets/Scripts/Systems/ProjectileSystem.cs
@@ -0,0 +1,69 @@
+﻿using Netologia.Behaviours;
+using Netologia.TowerDefence;
+using Netologia.TowerDefence.Behaviors;
+using UnityEngine;
+using Zenject;
+
+namespace Netologia.Systems
+{
+	public class ProjectileSystem : GameObjectPoolContainer<Projectile>, Director.IManualUpdate
+	{
+		private EffectSystem _effects;		//injected
+		
+		[SerializeField, Min(0.01f)]
+		private float _hitDistance = 0.3f;
+
+        //Логика ProjectileSystem
+        public void ManualUpdate()
+        {
+            var delta = TimeManager.DeltaTime;
+            foreach (var pool in this)
+            {
+                foreach (var projectile in pool)
+                {
+                    var transform = projectile.transform;
+                    var position = transform.position;
+                    var target = projectile.TargetPosition;
+
+                    var direction = Vector3.Normalize(target - position);
+                    position += direction * (projectile.MoveSpeed * delta);
+                    transform.up = direction;
+                    transform.position = position;
+
+                    if (Vector3.SqrMagnitude(position - target) <= _hitDistance)
+                    {
+                        if (projectile.HasEffect) //Эффект пули при попадании 
+                        {
+                            var effect = _effects[projectile.HitEffect].Get;
+                            effect.transform.position = position;
+                            effect.Play();  //У эффекта должно быть выставлено StopAction -> Disable
+                                            //У его системы галочка DisableTracking должна быть снята (Pools)
+                        }
+                        if (projectile.HasSound)
+                            AudioManager.PlayHit(projectile.HitSound); //Звуковой эффект
+
+                        projectile.DealDamage();
+                        this[projectile.Ref].ReturnElement(projectile.ID);
+                    }
+
+                }
+            }
+        }
+
+        public void OnDespawnUnit(int unitID)
+		{
+			foreach (var pool in this)
+				foreach (var projectile in pool)
+					if(projectile.TargetID == unitID)
+						projectile.ResetTarget();
+		}
+
+		[Inject]
+		private void Construct(EffectSystem effects)
+		{
+			(_effects) = (effects);
+			//SqrtMagnitude optimization
+			_hitDistance *= _hitDistance;
+		}
+	}
+}
---
+++ Assets/Scripts/Systems/TowerSystem.cs
@@ -0,0 +1,53 @@
+﻿using Netologia.Behaviours;
+using Netologia.TowerDefence;
+using Netologia.TowerDefence.Behaviors;
+using Zenject;
+
+namespace Netologia.Systems
+{
+	public class TowerSystem : GameObjectPoolContainer<Tower>, Director.IManualUpdate
+	{
+		private UnitSystem _units;				//injected
+		private ProjectileSystem _projectiles;  //injected
+
+        //Логика TowerSystem
+        public void ManualUpdate()
+        {
+            var delta = TimeManager.DeltaTime;
+            foreach (var pool in this)
+            {
+                foreach (var tower in pool)
+                {
+                    if (!tower.DecrementAttackReload(delta))
+                        continue;
+
+                    var position = tower.transform.position;
+                    if (!tower.HasTarget)
+                        tower.Target = _units.FindTarget(position, tower.Range);
+
+                    if (!tower.HasTarget) continue;
+
+                    var projectile = _projectiles[tower.Projectile].Get;
+                    projectile.PrepareData(position, tower.Target, tower.Damage, tower.AttackElemental);
+
+                    tower.Attack();
+                }
+            }
+        }
+
+        public void OnDespawnUnit(int unitID)
+		{
+			foreach (var pair in this)
+				foreach (var tower in pair)
+					if (tower.TargetID == unitID)
+						tower.Target = null;
+		}
+		
+		[Inject]
+		private void Construct(UnitSystem units, ProjectileSystem projectiles)
+		{
+			_units = units;
+			_projectiles = projectiles;
+		}
+	}
+}
